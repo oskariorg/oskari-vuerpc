2a7931b7a44810846fc7bc487b73015e63e3c7bb
nyholmi <ilkka.nyholm@maanmittauslaitos.fi>
2017-11-10 09:28:28 +0200

    new link

--- src/components/ui-components/iframe.vue
+++ src/components/ui-components/iframe.vue
@@ -2,7 +2,7 @@
 <div class="col-xs-12 col-md-12 col-sm-12 col-lg-6 col-xl-5 full-height-panel">
   <div class="panel panel-default map-panel">
     <div class="panel-body">
-      <iframe ref="publishedMap" id="publishedMap" src="http://demo.paikkatietoikkuna.fi/published/fi/0fcfd236-5079-49f3-81f3-58af23f534d2" style="border:none;
+      <iframe ref="publishedMap" id="publishedMap" src="https://kartta.paikkatietoikkuna.fi/?lang=en&uuid=053027f4-91d9-4351-aec4-c6a31dd68c56" style="border:none;
         width:100%; height:100%;"></iframe>
     </div>
    </div>
