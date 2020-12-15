21019b6dbd8329c349455aab6b073d2c290ae610
Sami Mäkinen <sami.makinen@nls.fi>
2020-12-15 17:35:21 +0200

    Cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,3 @@
-/*
-export {default as ThingA} from './ThingA'
-export {default as ThingB} from './ThingB'
-export {default as ThingC} from './ThingC'
-*/
 import gettingStarted from '../components/GettingStarted.vue';
 import map from './map';
 import marker from './marker';
@@ -28,17 +23,3 @@ const pageMap = {
 };
 
 export default pageMap;
-
-
-
-/*
-import mapClicked from './components/events/map.clicked.vue';
-import markerAfterAdd from './components/events/marker.added.vue';
-import markerClicked from './components/events/marker.clicked.vue';
-import featureEvent from './components/events/feature.event.vue';
-import drawingEvent from './components/events/drawing.event.vue';
-import feedbackResult from './components/events/feedback.result.vue';
-import routeResult from './components/events/route.result.vue';
-import seachResult from './components/events/search.result.vue';
-import userLocationEvent from './components/events/user.location.event.vue';
-*/
