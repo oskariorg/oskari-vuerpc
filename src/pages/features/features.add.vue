7c1f19362f98cede0f285c0730029bee0d2deafc
Sami Mäkinen <sami.makinen@nls.fi>
2021-02-01 17:50:03 +0200

    Simplify basic feature adding

--- src/pages/features/features.add.vue
+++ src/pages/features/features.add.vue
@@ -1,23 +1,42 @@
 <template>
   <div>
     <h2>{{ title }}</h2>
-    <p>AddFeaturesToMapRequest allows adding vector features on the map.</p>
+    <p>AddFeaturesToMapRequest allows adding vector features like points, lines and polygons on the map.</p>
     <DocumentationLink type="request" :apiDoc="apiDocPageRequestAdd">Documentation for {{ requestNameAdd }}</DocumentationLink>
 
-    <CodeSnippet :snippet="add" />
-      
-    <RunExampleButton @click="addFeaturesToMapRequest">{{ requestNameAdd }}</RunExampleButton>
-    <RunExampleButton @click="removeFeaturesFromMapRequest">{{requestNameRemove}}</RunExampleButton>
+    <h3>Add point to map</h3>
+    <CodeSnippet>
+var geojson = {{ JSON.stringify(pointGeoJSON, null, 2) }};
+// push vector features on geojson to map
+channel.postRequest('{{ requestNameAdd }}', [geojson]);
+    </CodeSnippet>
 
-    <h3>Update style</h3>
-    <p>You can update feature style for example when you want to higlight feature from the map. </p>
-    <RunExampleButton @click="updateFeatures">AddFeaturesToMapRequest (update feature)</RunExampleButton>
-    <CodeSnippet :snippet="update" />
+    <RunExampleButton @click="addPointToMap">Add point to map</RunExampleButton>
+    <p>
+      When you run the above code the vector feature is added to the map. 
+      However the map isn't moved to show the features by default.
+    </p>
+    <RunExampleButton @click="moveMapToPoint">Center map to show the point</RunExampleButton>
 
-    <h3>Using SVG for visualization for points</h3>
-    <p>Adding features with SVG icon (Oskari SVG-shape numbers: 0 - 6) </p>
-    <RunExampleButton @click="addFeaturesToMapRequestUsingSVG">{{ requestNameAdd }} (display point as SVG icon)</RunExampleButton>
-    <CodeSnippet :snippet="svg" />
+    <h3>Add point to map and show it</h3>
+    <p>
+      A common use case is that you want to show the added feature(s) immediately and you can do it by
+      giving another parameter for the request:
+    </p>
+    <CodeSnippet>
+channel.postRequest('{{ requestNameAdd }}', 
+  [geojson, {{ JSON.stringify(centerToGeomOpts)}}]);
+    </CodeSnippet>
+    <RunExampleButton @click="addPointToMap(true)">Add point and move map</RunExampleButton>
+    <p>
+      However this introduces another problem where especially with point features the map can be zoomed "too close". 
+      This can be prevented by using ZoomToFeaturesRequest instead:
+    </p>
+    <CodeSnippet>
+channel.postRequest('MapModulePlugin.ZoomToFeaturesRequest', 
+  [{ maxZoomLevel: 4 }]);
+    </CodeSnippet>
+    <RunExampleButton @click="zoomToPoint">Zoom to point (limited by zoom level)</RunExampleButton>
 
     <h3>Removing features</h3>
     <p>Remove all features across all vector layers</p>
@@ -27,29 +46,50 @@
     </CodeSnippet>
     <RunExampleButton @click="removeFeaturesFromMapRequest">{{ requestNameRemove }}</RunExampleButton>
 
-    <p>
-      Remove specific vector features from specific layer. 
-      In this example we are remove features from layer with id: {{ vectorLayerName }} that 
-      have an attribute named 'test_property' with value of 1.
+    <p>You can also use another toggle on the "{{ requestNameAdd }}"
+      parameters: "clearPrevious" to clear existing features from the map before adding new features:
     </p>
     <CodeSnippet>
-// Remove all feature's whose 'test_property' === 1 from the layer with id==='MY_VECTOR_LAYER'
-channel.postRequest('{{ requestNameRemove }}',['test_property', 1, '{{ vectorLayerName }}']);
+channel.postRequest('{{ requestNameAdd }}', 
+  [geojson, { "clearPrevious": true }]);
     </CodeSnippet>
-    <RunExampleButton @click="addFeaturesToMapRequest">{{ requestNameAdd }}</RunExampleButton>
-    <RunExampleButton @click="removeFeaturesFromMapRequest2">{{ requestNameRemove }}</RunExampleButton>
+
+    <h3>Events to react to</h3>
+    <p>
+      When clicking the example buttons on this page you get "FeatureEvent" lines in the log-panel.
+      When a feature is added, removed or clicked on the map the event is triggered which lets you react to it.
+      You can ensure that the features were correctly added to the map for the user, get a generated id for the
+      features to reference them by later on.
+    </p>
+    <DocumentationLink type="event" :apiDoc="apiDocPageEvent">Documentation for FeatureEvent</DocumentationLink>
+
+    <h3>Note!</h3>
+    <p>
+      Using the "{{ requestNameAdd }}" without referencing a layer in extra parameters a default layer is generated and used instead.
+      On later examples you can see how to use different layers for features can be used for more complex interaction and styling of features.
+      You can also see a layerId referenced on the FeatureEvent.
+    </p>
+
   </div>
 </template>
 
 <script>
-import { features } from '../../util/examplecodes.js';
+import { generator } from './vectorlayer_helpers';
 
 const title = 'Add/remove vector features';
 const requestNameAdd = 'MapModulePlugin.AddFeaturesToMapRequest';
 const requestNameRemove = 'MapModulePlugin.RemoveFeaturesFromMapRequest';
 const apiDocPageRequestAdd = 'mapping/mapmodule/request/addfeaturestomaprequest.md';
 const apiDocPageRequestRemove = 'mapping/mapmodule/request/removefeaturesfrommaprequest.md';
-const vectorLayerName = 'MY_VECTOR_LAYER';
+const apiDocPageEvent = 'mapping/mapmodule/event/featureevent.md';
+
+const x = 488704;
+const y = 6939136;
+//const geomPolygon = generator.getPolygon(x, y, { 'name': `I'm a polygon` });
+const geomPoint = generator.getPoint(x + 40000, y + 30000, { 'name': `I'm a point` });
+//const geojsonObject = generator.getCollectionOf([geomPolygon, geomPoint]);
+const pointGeoJSON = generator.getCollectionOf([geomPoint]);
+const centerToGeomOpts = { centerTo: true };
 
 export default {
   name: 'AddFeatures',
@@ -57,291 +97,57 @@ export default {
   data () {
     return {
       title,
-      svg: features.svg,
-      add: features.add,
-      update: features.update,
+      pointGeoJSON,
+      centerToGeomOpts,
       requestNameAdd,
       requestNameRemove,
       apiDocPageRequestAdd,
       apiDocPageRequestRemove,
-      vectorLayerName
+      apiDocPageEvent
     }
   },
+  beforeDestroy: () => {
+    // Clean up when user leaves the example
+    // NOTE! We don't have this.$root here so relying on global channel variable
+    // const channel = this.$root.channel;
+    channel.postRequest('MapModulePlugin.RemoveFeaturesFromMapRequest', []);
+  },
   methods: {
-    addFeaturesToMapRequest () {
-      const layerOptions = {
-        layerId: 'MY_VECTOR_LAYER',
-        layerInspireName: 'Inspire theme name',
-        layerOrganizationName: 'Organization name',
-        showLayer: true,
-        opacity: 100,
-        layerName: 'Layer name',
-        layerDescription: 'Description text',
-        layerPermissions: {
-          'publish': 'publication_permission_ok'
-        },
-        maxScale: 1,
-        minScale: 1451336,
-        hover: {
-          featureStyle: {
-            fill: {
-              color: '#ff00ff'
-            },
-            stroke: {
-              color: '#000000'
-            },
-            image: {
-              fill: {
-                color: '#ff0000'
-              },
-              radius: 10
-            }
-          },
-          content: [
-              { 'key': 'Layer: MY_VECTOR_LAYER' },
-              { 'key': 'Test property', 'valueProperty': 'test_property' }
-          ]
-        }
+    addPointToMap (moveMap = false) {
+      // remove any previous points
+      // this.$root.channel.postRequest('MapModulePlugin.RemoveFeaturesFromMapRequest', []);
+      // add point to the map
+      let opts = {
+        "clearPrevious": true
       };
-      this.$root.channel.postRequest('VectorLayerRequest', [layerOptions]);
-      this.$root.channel.log('VectorLayerRequest posted with data', [layerOptions]);
-
-      const x = 488704;
-      const y = 6939136;
-      const geojsonObject = {
-        'type': 'FeatureCollection',
-        'crs': {
-          'type': 'name',
-          'properties': {
-            'name': 'EPSG:3067'
-          }
-        },
-        'features': [
-          {
-            'type': 'Feature',
-            'geometry': {
-              'type': 'Polygon',
-              'coordinates': [[[x, y], [x + 100000, y + 100000], [x + 25000, y + 50000]]]
-            },
-            'properties': {
-              'test_property': 1
-            }
-          },
-          {
-            'type': 'Feature',
-            'geometry': {
-              'type': 'Point',
-              'coordinates': [x + 40000, y + 30000]
-            },
-            'properties': {
-              'test_property': 2
-            }
-          }
-
-        ]
-      };
-      const params = [geojsonObject, {
-        layerId: 'MY_VECTOR_LAYER',
-        clearPrevious: true,
-        centerTo: true,
-        cursor: 'zoom-in',
-        prio: 4,
-        featureStyle: {
-          text: {
-            scale: 1.3,
-            fill: {
-              color: 'rgba(0,0,0,1)'
-            },
-            stroke: {
-              color: 'rgba(255,255,255,1)',
-              width: 2
-            },
-            labelProperty: 'test_property'
-          }
-        },
-        optionalStyles: [{
-          property: { key: 'test_property', value: 2 },
-          stroke: {
-            color: '#FFFF00'
-          },
-          image: {
-            fill: {
-              color: 'rgba(0,0,0,1)'
-            },
-            radius: 4
-          }
-        }]
-      }];
-      this.$root.channel.postRequest(
-        'MapModulePlugin.AddFeaturesToMapRequest',
-        params
-      );
+      if (moveMap) {
+        opts = {
+          ...centerToGeomOpts,
+          ...opts
+        };
+      }
+      const params = [pointGeoJSON, opts];
+      this.$root.channel.postRequest(requestNameAdd, params);
       this.$root.channel.log('MapModulePlugin.AddFeaturesToMapRequest posted with data', params);
-
-      // Next add features on default vector layer
-      const defaultVectorLayerOptions = {
-        hover: {
-          featureStyle: {
-            'inherit': true,
-            'effect': 'auto major'
-          },
-          content: [
-              { 'key': 'Layer: default' },
-              { 'key': 'Test property', 'valueProperty': 'test_property' }
-          ]
-        }
-      };
-      this.$root.channel.postRequest('VectorLayerRequest', [defaultVectorLayerOptions]);
-      this.$root.channel.log('VectorLayerRequest posted with data', [defaultVectorLayerOptions]);
-
-      const geojsonObject2 = {
-        'type': 'FeatureCollection',
-        'crs': {
-          'type': 'name',
-          'properties': {
-            'name': 'EPSG:3067'
-          }
-        },
-        'features': [
-          {
-            'type': 'Feature',
-            'geometry': {
-              'type': 'LineString',
-              'coordinates': [[x + 30000, y], [x + 130000, y + 100000]]
-            },
-            'properties': {
-              'test_property': 'Line'
-            }
-          },
-          {
-            'type': 'Feature',
-            'geometry': {
-              'type': 'Point',
-              'coordinates': [x + 30000, y]
-            },
-            'properties': {
-              'test_property': 'Point'
-            }
-          }
-
-        ]
-      };
-
-      const params2 = [geojsonObject2, {
-        clearPrevious: false,
-        centerTo: true,
-        featureStyle: {
-          fill: {
-            color: '#ff0000'
-          },
-          stroke: {
-            color: '#ff0000',
-            width: 5
-          },
-          text: {
-            scale: 1.3,
-            fill: {
-              color: 'rgba(0,0,0,1)'
-            },
-            stroke: {
-              color: 'rgba(255,255,255,1)',
-              width: 2
-            },
-            labelProperty: 'test_property'
-          }
-        },
-        cursor: 'zoom-out',
-        prio: 1
-      }];
-
-      this.$root.channel.postRequest(
-        'MapModulePlugin.AddFeaturesToMapRequest',
-        params2
-      );
-      this.$root.channel.log('MapModulePlugin.AddFeaturesToMapRequest posted with data', params2);
     },
-    updateFeatures () {
-      // Define the feature to be updated
-      var updateFeature = {'test_property': 1};
 
-      var params = [updateFeature, {
-        layerId: 'MY_VECTOR_LAYER',
-        featureStyle: {
-          fill: {
-            color: '#FF0000'
-          }
-        },
-        animationDuration: 500
-      }];
-      this.$root.channel.postRequest(
-        'MapModulePlugin.AddFeaturesToMapRequest',
-        params
-      );
+    moveMapToPoint () {
+      // point is offset from x/y
+      const params = [x + 40000, y + 30000];
+      this.$root.channel.postRequest('MapMoveRequest', params);
+      this.$root.channel.log('MapMoveRequest posted with data', params);
     },
+
+    zoomToPoint () {
+      var maxZoomParams = {
+        maxZoomLevel: 4
+      };
+      this.$root.channel.postRequest('MapModulePlugin.ZoomToFeaturesRequest', [maxZoomParams]);
+    },
+
     removeFeaturesFromMapRequest () {
       this.$root.channel.postRequest('MapModulePlugin.RemoveFeaturesFromMapRequest', []);
       this.$root.channel.log('MapModulePlugin.RemoveFeaturesFromMapRequest posted without params');
-    },
-    removeFeaturesFromMapRequest2 () {
-      const params = ['test_property', 1, 'MY_VECTOR_LAYER'];
-      this.$root.channel.postRequest('MapModulePlugin.RemoveFeaturesFromMapRequest', params);
-      this.$root.channel.log('MapModulePlugin.RemoveFeaturesFromMapRequest posted with params', params);
-    },
-    addFeaturesToMapRequestUsingSVG () {
-      const geojsonObject = {
-        'type': 'FeatureCollection',
-        'crs': {
-          'type': 'name',
-          'properties': {
-            'name': 'EPSG:3067'
-          }
-        },
-        'features': [
-          {
-            'type': 'Feature',
-            'geometry': {
-              'type': 'Point',
-              'coordinates': [500000, 6939136]
-            },
-            'properties': {
-              'label': 'I am a point feature!',
-              'test_property': 'SVG'
-            }
-          }
-        ]
-      };
-      const params = [geojsonObject, {
-        clearPrevious: false,
-        centerTo: true,
-        cursor: 'zoom-in',
-        featureStyle: {
-          image: {
-            shape: 2,
-            size: 1,
-            fill: {
-              color: '#ff3300'
-            },
-            stroke: {
-              color: '#000000'
-            }
-          },
-          text: {
-            scale: 1.3,
-            fill: {
-              color: 'rgba(0,0,0,1)'
-            },
-            stroke: {
-              color: 'rgba(255,255,255,1)',
-              width: 2
-            },
-            labelProperty: 'label',
-            offsetX: 65,
-            offsetY: 8
-          }
-        }
-      }];
-      this.$root.channel.postRequest('MapModulePlugin.AddFeaturesToMapRequest', params);
-      this.$root.channel.log('MapModulePlugin.AddFeaturesToMapRequest posted with data', params);
     }
   }
 }
