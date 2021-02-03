a15f4188b27bec1ef76e1749091402707388a60d
Sami Mäkinen <sami.makinen@nls.fi>
2021-02-03 18:07:12 +0200

    Fix indentation

--- src/util/channelLogger.js
+++ src/util/channelLogger.js
@@ -28,27 +28,27 @@ const createLogger = (logStore) => {
 const createOnReady = (channel, expectedOskariVersion) => {
     channel.enableDebug(true);
     channel.onReady(() => {
-    channel.log('Map is now listening');
+        channel.log('Map is now listening');
 
-    channel.isSupported(expectedOskariVersion, (blnSupported) => {
-        if (blnSupported) {
-          channel.log('Client is supported and Oskari version is ' + expectedOskariVersion);
-        } else {
-          channel.log('Oskari-instance is not the one we expect (' + expectedOskariVersion + ') or client not supported');
-          // getInfo can be used to get the current Oskari version
-          channel.getInfo((oskariInfo) => {
-            channel.log('Current Oskari-instance reports version as: ', oskariInfo);
-          });
-        }
-      });
-      channel.isSupported((blnSupported) => {
-        if (!blnSupported) {
-          channel.log('Oskari reported client version is not supported: ' + OskariRPC.VERSION +
-          '. The client might work, but some features are not backward compatible.');
-        } else {
-          channel.log('Client is supported by Oskari.');
-        }
-      });
+        channel.isSupported(expectedOskariVersion, (blnSupported) => {
+          if (blnSupported) {
+            channel.log('Client is supported and Oskari version is ' + expectedOskariVersion);
+          } else {
+            channel.log('Oskari-instance is not the one we expect (' + expectedOskariVersion + ') or client not supported');
+            // getInfo can be used to get the current Oskari version
+            channel.getInfo((oskariInfo) => {
+              channel.log('Current Oskari-instance reports version as: ', oskariInfo);
+            });
+          }
+        });
+        channel.isSupported((blnSupported) => {
+          if (!blnSupported) {
+            channel.log('Oskari reported client version is not supported: ' + OskariRPC.VERSION +
+            '. The client might work, but some features are not backward compatible.');
+          } else {
+            channel.log('Client is supported by Oskari.');
+          }
+        });
     });
   }
 
