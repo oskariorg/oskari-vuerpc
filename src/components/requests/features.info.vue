9cde066c2dd101ecb80aa0ed297ca63160895f4b
kasparia <superkasperi@gmail.com>
2021-03-10 14:24:27 +0200

    Set layer default visibility to true

--- src/components/requests/features.info.vue
+++ src/components/requests/features.info.vue
@@ -2,10 +2,9 @@
   <div ref="GetFeatureInfoRequest">
     To show feature info box for a certain layer you must ensure first that the layer visibility is set to true and then proceed to send GetFeatureInfoRequest.
     <br><br>
-    1. Set visibility of wanted layer as true.
-    <button id="btnMapModulePluginLayerVisibilityRequest" class="btn btn-primary exampleready" @click="mapLayerVisibilityRequest">MapModulePlugin.MapLayerVisibilityRequest</button>
-    <code-component snippet="channel.postRequest('MapModulePlugin.MapLayerVisibilityRequest', [492, true]);
-  channel.log('MapModulePlugin.MapLayerVisibilityRequest posted with data', [492, true]);"></code-component>
+    1. Set visibility of wanted layer as true. In this example visibility is set to true already.
+    <CodeSnippet snippet="channel.postRequest('MapModulePlugin.MapLayerVisibilityRequest', [492, true]);
+  channel.log('MapModulePlugin.MapLayerVisibilityRequest posted with data', [492, true]);"></CodeSnippet>
 
     2. Show feature info box
     <button id="btnMapModulePluginGetFeatureInfoRequest" class="btn btn-primary exampleready" @click="getFeatureInfoRequest">MapModulePlugin.GetFeatureInfoRequest</button>
@@ -13,15 +12,20 @@
       <a id="getFeatureInfo" href="" @click="getLink">To the documentation</a>
     </div>
 
-    <code-component snippet="//get map center and display and feature info box
+    <CodeSnippet snippet="//get map center and display and feature info box
   channel.getMapPosition(function(data){
     var lonlat = [data.centerX, data.centerY];
     channel.postRequest('MapModulePlugin.GetFeatureInfoRequest', lonlat);
     channel.log('MapModulePlugin.GetFeatureInfoRequest posted with data', lonlat);
-  });"></code-component>
+  });"></CodeSnippet>
   </div>
 </template>
 <script>
+import EVENTBUS from '../../util/eventbus.js';
+
+const listeners = [];
+let layerVisibility = false;
+
 export default {
   name: 'GetFeatureInfoRequest',
   label: 'Get feature info',
@@ -31,8 +35,9 @@ export default {
     }
   },
   methods: {
-    mapLayerVisibilityRequest () {
-      this.$root.channel.postRequest('MapModulePlugin.MapLayerVisibilityRequest', [492, true]);
+    mapLayerVisibilityRequest (showLayer) {
+      layerVisibility = showLayer;
+      this.$root.channel.postRequest('MapModulePlugin.MapLayerVisibilityRequest', [492, showLayer]);
       this.$root.channel.log('MapModulePlugin.MapLayerVisibilityRequest posted with data', [492, true]);
     },
     getFeatureInfoRequest () {
@@ -46,6 +51,30 @@ export default {
       let documentPathEnd = 'mapping/mapmodule/request/GetFeatureInfoRequest.md';
       e.target.href = this.$root.documentPathRequest + documentPathEnd;
     }
+  },
+  mounted () {
+    if (this.$root.channel && !layerVisibility) {
+      this.mapLayerVisibilityRequest(true);
+    }
+    listeners.push(EVENTBUS.on('channel.available', (event) => {
+      if (!layerVisibility) {
+        this.mapLayerVisibilityRequest(true);
+      }
+    }));
+  },
+  updated () {
+
+  },
+  beforeDestroy () {
+    this.mapLayerVisibilityRequest(false);
+    while (listeners.length) {
+      EVENTBUS.off('rpcAppDisplayMessage', listeners.pop());
+    }
   }
 }
+
+const toggleLayerVisibility = () => {
+  this.$root.channel.postRequest('MapModulePlugin.MapLayerVisibilityRequest', [492, true]);
+  this.$root.channel.log('MapModulePlugin.MapLayerVisibilityRequest posted with data', [492, true]);
+};
 </script>
