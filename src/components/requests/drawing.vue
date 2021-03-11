3bd9c3a967c7594be3d40facb374d5ef4373be44
kasparia <superkasperi@gmail.com>
2021-03-11 13:48:17 +0200

    Pr comment fixes

--- src/components/requests/drawing.vue
+++ src/components/requests/drawing.vue
@@ -1,52 +1,67 @@
-<style scoped>
-  code {
-    font-size: 100%;
-  }
-</style>
 <template>
-  <div ref="drawing">
-    <div id="StartDrawingRequest">
+  <div>
+    <div>
       To start drawing send <code>StartDrawingRequest</code>.
-      <button class="btn btn-primary exampleready" @click="startDrawing">DrawTools.StartDrawingRequest</button>
+      <RunExampleButton class="btn btn-primary exampleready" @click="startDrawing">DrawTools.StartDrawingRequest</RunExampleButton>
       <div>
-        <a id="startDrawing" href="" @click="getLink">To the documentation</a>
+        <DocumentationLink type="request" apiDoc="mapping/drawtools/request/startdrawingrequest.md">Documentation for {{ startDrawingRequest }}</DocumentationLink>
       </div>
-      <code-component snippet="var data = ['my functionality id', 'Polygon'];
-      channel.postRequest('DrawTools.StartDrawingRequest', data);"></code-component>
+      <CodeSnippet snippet="var data = ['my functionality id', 'Polygon'];
+      channel.postRequest('DrawTools.StartDrawingRequest', data);"></CodeSnippet>
     </div>
-    <div id="StopDrawingRequest">
+    <div>
       To stop current drawing progress send <code>StopDrawingRequest</code>.
-      <button class="btn btn-primary exampleready" @click="stopDrawing">DrawTools.StopDrawingRequest</button>
+      <RunExampleButton class="btn btn-primary exampleready" @click="stopDrawing">DrawTools.StopDrawingRequest</RunExampleButton>
       <div>
-        <a id="stopDrawing" href="" @click="getLink">To the documentation</a>
+        <DocumentationLink type="request" apiDoc="mapping/drawtools/request/stopdrawingrequest.md">Documentation for {{ stopDrawingRequest }}</DocumentationLink>
       </div>
-      <code-component snippet="var data = ['my functionality id'];
-      channel.postRequest('DrawTools.StopDrawingRequest', data);"></code-component>
+      <CodeSnippet snippet="var data = ['my functionality id'];
+      channel.postRequest('DrawTools.StopDrawingRequest', data);"></CodeSnippet>
     </div>
-    <div id="StopDrawingRequestClear">
+    <div>
       To clear drawing from the map send <code>StopDrawingRequest</code> with second member of data parameter given to StopDrawingRequest set as true.
-      <button class="btn btn-primary exampleready" @click="stopDrawingClear">DrawTools.StopDrawingRequest - clear drawings</button>
-      <code-component snippet="var data = ['my functionality id', true];
-      channel.postRequest('DrawTools.StopDrawingRequest', data);"></code-component>
+      <RunExampleButton class="btn btn-primary exampleready" @click="stopDrawingClear">DrawTools.StopDrawingRequest - clear drawings</RunExampleButton>
+      <CodeSnippet snippet="var data = ['my functionality id', true];
+      channel.postRequest('DrawTools.StopDrawingRequest', data);"></CodeSnippet>
     </div>
-    <div id="DrawingEvent">
+    <div>
       Everytime mouse is moved on the map DrawingEvent occurs while drawing is in progress and contains all the information regarding current drawing. To ensure that logging isn't clogged check if <code>DrawingEvent.finished</code> is set to <code>true</code> and only log when drawing is finished.
       <div>
-        <a id="drawingEvent" href="" @click="getLink">To the documentation</a>
+        <DocumentationLink type="event" apiDoc="mapping/drawtools/event/DrawingEvent.md">Documentation for {{ drawingEventRequest }}</DocumentationLink>
       </div>
-      <code-component :snippet="event"></code-component>
+      <CodeSnippet :snippet="event"></CodeSnippet>
     </div>
   </div>
 </template>
 <script>
-import { drawing } from '../../util/examplecodes.js';
+const drawingEventRequest = 'DrawingEvent';
+const startDrawingRequest = 'DrawTools.StartDrawingRequest';
+const stopDrawingRequest = 'DrawTools.StopDrawingRequest';
+
+const drawingExample = {
+  event: `{
+    "name": "DrawingEvent",
+    "id": "my functionality id",
+    "geojson": "{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[356352,6776320],[428032,6776064]]]},\"properties\":{\"area\":\"0 m2\"}}]}",
+    "data": {
+      "area": "0 m2",
+      "bufferedGeoJson": "{\"type\":\"FeatureCollection\",\"features\":[]}",
+      "shape": "Polygon"
+    },
+    "isFinished": true
+  }`
+}
+
 export default {
   name: 'drawing',
   label: 'Drawing requests',
   data () {
     return {
+      drawingEventRequest,
+      startDrawingRequest,
+      stopDrawingRequest,
       desc: 'Drawing requests',
-      event: drawing.event
+      event: drawingExample.event
     }
   },
   methods: {
@@ -64,15 +79,12 @@ export default {
       const data = ['my functionality id', true];
       this.$root.channel.postRequest('DrawTools.StopDrawingRequest', data);
       this.$root.channel.log('DrawTools.StopDrawingRequest posted with data:', data);
-    },
-    getLink (e) {
-      let documentPathEnd =
-        e.target.id === 'startDrawing' ? 'mapping/drawtools/request/startdrawingrequest.md' :
-        e.target.id === 'stopDrawing' ? 'mapping/drawtools/request/stopdrawingrequest.md' :
-        'mapping/drawtools/event/DrawingEvent.md';
-      let requestBaseUrl = e.target.id === 'drawingEvent' ? this.$root.documentPathEvent : this.$root.documentPathRequest;
-      e.target.href = requestBaseUrl + documentPathEnd;
     }
   }
 }
 </script>
+<style scoped>
+  code {
+    font-size: 100%;
+  }
+</style>
