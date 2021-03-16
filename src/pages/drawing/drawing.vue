0f150eea54ad07058653c5dfacfd7d0feab7a01f
kasparia <superkasperi@gmail.com>
2021-03-16 12:08:43 +0200

    Unify and fix documentation

--- src/pages/drawing/drawing.vue
+++ src/pages/drawing/drawing.vue
@@ -6,30 +6,52 @@
       <div>
         <DocumentationLink type="request" apiDoc="mapping/drawtools/request/startdrawingrequest.md">Documentation for {{ startDrawingRequest }}</DocumentationLink>
       </div>
-      <CodeSnippet snippet="var data = ['my functionality id', 'Polygon'];
-      channel.postRequest('DrawTools.StartDrawingRequest', data);"></CodeSnippet>
+      <CodeSnippet>
+        var data = ['my functionality id', 'Polygon'];
+        channel.postRequest('DrawTools.StartDrawingRequest', data);
+      </CodeSnippet>
     </div>
+
     <div>
       To stop current drawing progress send <code>StopDrawingRequest</code>.
       <RunExampleButton class="btn btn-primary exampleready" @click="stopDrawing">DrawTools.StopDrawingRequest</RunExampleButton>
       <div>
         <DocumentationLink type="request" apiDoc="mapping/drawtools/request/stopdrawingrequest.md">Documentation for {{ stopDrawingRequest }}</DocumentationLink>
       </div>
-      <CodeSnippet snippet="var data = ['my functionality id'];
-      channel.postRequest('DrawTools.StopDrawingRequest', data);"></CodeSnippet>
+      <CodeSnippet>
+        var data = ['my functionality id'];
+        channel.postRequest('DrawTools.StopDrawingRequest', data);
+      </CodeSnippet>
     </div>
+
     <div>
       To clear drawing from the map send <code>StopDrawingRequest</code> with second member of data parameter given to StopDrawingRequest set as true.
       <RunExampleButton class="btn btn-primary exampleready" @click="stopDrawingClear">DrawTools.StopDrawingRequest - clear drawings</RunExampleButton>
-      <CodeSnippet snippet="var data = ['my functionality id', true];
-      channel.postRequest('DrawTools.StopDrawingRequest', data);"></CodeSnippet>
+      <CodeSnippet>
+        var data = ['my functionality id', true];
+      channel.postRequest('DrawTools.StopDrawingRequest', data);
+      </CodeSnippet>
     </div>
+
     <div>
       Everytime mouse is moved on the map DrawingEvent occurs while drawing is in progress and contains all the information regarding current drawing. To ensure that logging isn't clogged check if <code>DrawingEvent.finished</code> is set to <code>true</code> and only log when drawing is finished.
       <div>
         <DocumentationLink type="event" apiDoc="mapping/drawtools/event/DrawingEvent.md">Documentation for {{ drawingEventRequest }}</DocumentationLink>
       </div>
-      <CodeSnippet :snippet="event"></CodeSnippet>
+      <CodeSnippet>
+        {
+            "name": "DrawingEvent",
+            "id": "my functionality id",
+            "geojson": "{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[356352,6776320],[428032,6776064]]]},\"properties\":{\"area\":\"0 m2\"}}]}",
+            "data": {
+              "area": "0 m2",
+              "bufferedGeoJson": "{\"type\":\"FeatureCollection\",\"features\":[]}",
+              "shape": "Polygon"
+            },
+            "isFinished": true
+          }
+      </CodeSnippet>
+
     </div>
   </div>
 </template>
@@ -37,20 +59,6 @@
 const drawingEventRequest = 'DrawingEvent';
 const startDrawingRequest = 'DrawTools.StartDrawingRequest';
 const stopDrawingRequest = 'DrawTools.StopDrawingRequest';
-
-const drawingExample = {
-  event: `{
-    "name": "DrawingEvent",
-    "id": "my functionality id",
-    "geojson": "{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[356352,6776320],[428032,6776064]]]},\"properties\":{\"area\":\"0 m2\"}}]}",
-    "data": {
-      "area": "0 m2",
-      "bufferedGeoJson": "{\"type\":\"FeatureCollection\",\"features\":[]}",
-      "shape": "Polygon"
-    },
-    "isFinished": true
-  }`
-}
 
 export default {
   name: 'drawing',
@@ -60,8 +68,7 @@ export default {
       drawingEventRequest,
       startDrawingRequest,
       stopDrawingRequest,
-      desc: 'Drawing requests',
-      event: drawingExample.event
+      desc: 'Drawing requests'
     }
   },
   methods: {
--- src/pages/drawing/index.js
+++ src/pages/drawing/index.js
@@ -1,27 +1,8 @@
-/*
-export {default as ThingA} from './ThingA'
-export {default as ThingB} from './ThingB'
-export {default as ThingC} from './ThingC'
-*/
-import drawing from '../../components/requests/drawing.vue';
+import drawing from './drawing.vue';
 
 export default {
-    name: 'Drawing',
-    pages: [
-        drawing
-    ]
+  name: 'Drawing',
+  pages: [
+    drawing
+  ]
 };
-
-
-
-/*
-import mapClicked from './components/events/map.clicked.vue';
-import markerAfterAdd from './components/events/marker.added.vue';
-import markerClicked from './components/events/marker.clicked.vue';
-import featureEvent from './components/events/feature.event.vue';
-import drawingEvent from './components/events/drawing.event.vue';
-import feedbackResult from './components/events/feedback.result.vue';
-import routeResult from './components/events/route.result.vue';
-import seachResult from './components/events/search.result.vue';
-import userLocationEvent from './components/events/user.location.event.vue';
-*/
