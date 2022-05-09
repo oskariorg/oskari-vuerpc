f117ba790b8af4b1d98d57394b0681fd5fd5ba54
Sami Mäkinen <sami.makinen@nls.fi>
2022-05-09 17:56:15 +0300

    Moved to marker example page

--- src/util/examplecodes.js
+++ src/util/examplecodes.js
@@ -264,34 +264,4 @@ var infobox = {
     channel.log('InfoBox.ShowInfoBoxRequest posted with data', data);
   });`
 }
-var marker = {
-  addDefault: `var data = {
-    x: 386020,
-    y: 6670057,
-    color: 'ff0000',
-    msg : '',
-    shape: 3, // icon number (0-6)
-    size: 3
-  };
-  channel.postRequest('MapModulePlugin.AddMarkerRequest', [data, MARKER_ID]);`,
-  addSvg: `var data = {
-    x: 386020,
-    y: 6670057,
-    msg : '',
-    shape: '&#60;svg width="32" height="32">&#60;g fill="#9955ff" transform="matrix(0.06487924,0,0,0.06487924,0,1.73024e-6)">&#60;g>&#60;path d="M 246.613,0 C 110.413,0 0,110.412 0,246.613 c 0,136.201 110.413,246.611 246.613,246.611 136.2,0 246.611,-110.412 246.611,-246.611 C 493.224,110.414 382.812,0 246.613,0 Z m 96.625,128.733 c 21.128,0 38.256,17.128 38.256,38.256 0,21.128 -17.128,38.256 -38.256,38.256 -21.128,0 -38.256,-17.128 -38.256,-38.256 0,-21.128 17.128,-38.256 38.256,-38.256 z m -196.743,0 c 21.128,0 38.256,17.128 38.256,38.256 0,21.128 -17.128,38.256 -38.256,38.256 -21.128,0 -38.256,-17.128 -38.256,-38.256 0,-21.128 17.128,-38.256 38.256,-38.256 z m 100.738,284.184 c -74.374,0 -138.225,-45.025 -165.805,-109.302 l 48.725,0 c 24.021,39.5 67.469,65.885 117.079,65.885 49.61,0 93.058,-26.384 117.079,-65.885 l 48.725,0 C 385.46,367.892 321.608,412.917 247.233,412.917 Z" />&#60;/g>&#60;g/>&#60;g/>&#60;g/>&#60;g/>&#60;g/>&#60;g/>&#60;g/>&#60;g/>&#60;g/>&#60;g/>&#60;g/>&#60;g/>&#60;g/>&#60;g/>&#60;g/>&#60;/g>&#60;g transform="translate(0,-461.224)" />&#60;g transform="translate(0,-461.224)" />&#60;g transform="translate(0,-461.224)" />&#60;g transform="translate(0,-461.224)" />&#60;g transform="translate(0,-461.224)" />&#60;g transform="translate(0,-461.224)" />&#60;g transform="translate(0,-461.224)" />&#60;g transform="translate(0,-461.224)" />&#60;g transform="translate(0,-461.224)" />&#60;g transform="translate(0,-461.224)" />&#60;g transform="translate(0,-461.224)" />&#60;g transform="translate(0,-461.224)" />&#60;g transform="translate(0,-461.224)" />&#60;g transform="translate(0,-461.224)" />&#60;g transform="translate(0,-461.224)" />&#60;/svg>',
-    offsetX: 16, // center point x position from left to right
-    offsetY: 16, // center point y position from bottom to up
-    size: 3
-  };
-  channel.postRequest('MapModulePlugin.AddMarkerRequest', [data, MARKER_ID]);`,
-  addOwn: `var data = {
-    x: 386020,
-    y: 6670027,
-    shape: 'https://www.oskari.org/images/oskari_logo_white.png',
-    offsetX: 80, // center point x position from left to right
-    offsetY: -50 // center point y position from bottom to up
-  };
-  channel.postRequest('MapModulePlugin.AddMarkerRequest', [data, MARKER_ID]);`
-};
-
-export { features, wkt, feedback, infobox, marker, search };
+export { features, wkt, feedback, infobox, search };
