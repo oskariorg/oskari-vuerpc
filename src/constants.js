bb9b0672fc2a2bfe79817fb35b3afb0fcd5bd088
Aho Henri <vk585@maanmittauslaitos.fi>
2022-08-25 13:19:27 +0300

    StateChangedEvent description.

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const EXPECTED_OSKARI_VERSION = '2.7.0';
+export const EXPECTED_OSKARI_VERSION = '2.8.0';
 export const OSKARI_DOMAIN = 'https://kartta.paikkatietoikkuna.fi';
 export const OSKARI_EMBEDDED_MAP_UUID = '053027f4-91d9-4351-aec4-c6a31dd68c56';
--- src/pages/map/state.vue
+++ src/pages/map/state.vue
@@ -43,11 +43,130 @@ channel.resetState(function () {
     </CodeSnippet>
 
     <RunExampleButton @click="resetState">Reset state</RunExampleButton>
+
+    <h3>StateChangedEvent</h3>
+    <p>
+      Sent after massive application state change occurs. This allows RPC-based apps to detect things like user clicking on the "reset map to default". The state "reset" clears for example markers from the map and re-add for example markers that they need after such reset.
+    </p>
+    <DocumentationLink type="event" :apiDoc="eventApiDocPage">Documentation for {{eventName}}</DocumentationLink>
+    <CodeSnippet>
+{
+    "current": {
+        "mapfull": {
+            "state": {
+                "north": 7250000,
+                "east": 520000,
+                "zoom": 0,
+                "srs": "EPSG:3067",
+                "selectedLayers": [
+                    {
+                        "id": 801,
+                        "opacity": 100,
+                        "style": "default"
+                    }
+                ],
+                "plugins": {
+                    "MainMapModuleMarkersPlugin": {
+                        "markers": []
+                    },
+                    "MainMapModuleFullScreenPlugin": {
+                        "fullscreen": false
+                    }
+                }
+            }
+        },
+        "toolbar": {
+            "state": {}
+        },
+        "search": {
+            "state": {}
+        },
+        "statsgrid": {
+            "state": {
+                "view": false
+            }
+        },
+        "metadataflyout": {
+            "state": {}
+        },
+        "printout": {
+            "state": {}
+        },
+        "timeseries": {
+            "state": null
+        },
+        "maprotator": {
+            "state": {
+                "degrees": 0
+            }
+        }
+    },
+    "previous": {
+        "mapfull": {
+            "state": {
+                "north": 8009268.642888758,
+                "east": 99676.35560292119,
+                "zoom": 2,
+                "srs": "EPSG:3067",
+                "selectedLayers": [
+                    {
+                        "id": 801,
+                        "opacity": 100,
+                        "style": "default"
+                    }
+                ],
+                "plugins": {
+                    "MainMapModuleMarkersPlugin": {
+                        "markers": []
+                    },
+                    "MainMapModuleFullScreenPlugin": {
+                        "fullscreen": false
+                    }
+                }
+            }
+        },
+        "toolbar": {
+            "state": {
+                "selected": {
+                    "id": "select",
+                    "group": "default-basictools"
+                }
+            }
+        },
+        "search": {
+            "state": {}
+        },
+        "statsgrid": {
+            "state": {
+                "view": false
+            }
+        },
+        "metadataflyout": {
+            "state": {}
+        },
+        "printout": {
+            "state": {}
+        },
+        "timeseries": {
+            "state": null
+        },
+        "maprotator": {
+            "state": {
+                "degrees": 0
+            }
+        }
+    }
+}
+    </CodeSnippet>
+
   </div>
 </template>
 
 <script>
 const title = 'Handle map state';
+
+const eventApiDocPage = 'framework/core/event/StateSavedEvent.md';
+const eventName = 'StateChangedEvent';
 
 export default {
   name: 'MapState',
@@ -55,7 +174,9 @@ export default {
   data () {
     return {
       title,
-      savedState: {}
+      savedState: {},
+      eventApiDocPage,
+      eventName
     }
   },
   computed: {
