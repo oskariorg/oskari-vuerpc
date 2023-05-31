32812f8f4c745d6e02881713fe5c456673ea90bb
AleksiKaa <allu.kaariainen@gmail.com>
2023-05-31 15:06:24 +0300

    move all 'location' related pages to their own subfolder

--- src/pages/location/index.js
+++ src/pages/location/index.js
@@ -1,7 +1,7 @@
-import routeResult from '../../components/events/route.result.vue';
+import routeResult from './route.result.vue';
 import routeGet from './route.get.vue';
-import userLocation from '../../components/requests/user.location.vue';
-import userLocationEvent from '../../components/events/user.location.event.vue';
+import userLocation from './user.location.vue';
+import userLocationEvent from './user.location.event.vue';
 import search from './search.vue';
 
 export default {
