97ed0d2ced87f5712233d48199540ba7b4f67978
AleksiKaa <allu.kaariainen@gmail.com>
2023-05-31 16:10:16 +0300

    migrate file to use ui components

--- src/pages/location/user.location.vue
+++ src/pages/location/user.location.vue
@@ -1,17 +1,16 @@
 <template>
-  <div ref="GetUserLocationRequest">
-    <button
-      id="btnMyLocationPluginGetUserLocationRequest"
-      class="btn btn-primary"
-      @click="getUserLocation"
-    >
+  <div>
+    <RunExampleButton @click="getUserLocation">
       MyLocationPlugin.GetUserLocationRequest
-    </button>
-    <div>
-      <a id="getUserLocation" href="" @click="getLink">To the documentation</a>
-    </div>
-        <CodeSnippet>channel.postRequest('MyLocationPlugin.GetUserLocationRequest', [true]);</CodeSnippet>
-    </div>
+    </RunExampleButton>
+    <br />
+    <DocumentationLink type="request" :apiDoc="apiDocpage">
+      Documentation for GetUserLocationRequest
+    </DocumentationLink>
+    <CodeSnippet>
+      channel.postRequest('MyLocationPlugin.GetUserLocationRequest', [true]);
+    </CodeSnippet>
+  </div>
 </template>
 <script>
 export default {
@@ -19,17 +18,14 @@ export default {
   label: 'Get users location',
   data() {
     return {
-      desc: 'Get users location'
+      desc: 'Get users location',
+      apiDocpage: 'mapping/mapmodule/request/getuserlocationrequest.md'
     };
   },
   methods: {
     getUserLocation() {
       this.$root.channel.postRequest('MyLocationPlugin.GetUserLocationRequest', [true]);
       this.$root.channel.log('MyLocationPlugin.GetUserLocationRequest posted with data', true);
-    },
-    getLink(e) {
-      let documentPathEnd = 'mapping/mapmodule/request/getuserlocationrequest.md';
-      e.target.href = this.$root.documentPathRequest + documentPathEnd;
     }
   }
 };
