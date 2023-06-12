9d05a3992d909b2558fd4d682a19f4555b46a690
AleksiKaa <allu.kaariainen@gmail.com>
2023-06-12 12:57:43 +0300

    move variable from file

--- src/pages/features/features.layer.vue
+++ src/pages/features/features.layer.vue
@@ -124,7 +124,7 @@ channel.postRequest('{{ requestName }}', [{
   </div>
 </template>
 <script>
-import { LAYER_OPTS, generator } from './vectorlayer_helpers';
+import { generator } from './vectorlayer_helpers';
 
 const title = 'Configure layer for vector features';
 const requestName = 'VectorLayerRequest';
@@ -196,6 +196,40 @@ export default {
       this.$root.channel.postRequest('MapModulePlugin.RemoveFeaturesFromMapRequest', []);
       this.$root.channel.log('MapModulePlugin.RemoveFeaturesFromMapRequest posted without params');
     }
+  }
+};
+
+const LAYER_OPTS = {
+  simple: {
+    layerId: 'MY_VECTOR_LAYER',
+    opacity: 75,
+    // TODO: layer style can't be configured. Only hover style is supported
+    hover: {
+      featureStyle: {
+        // FIXME: inherit and effect don't work if features don't have styles specified in AddFeaturesToMap
+        //   inherit: true,
+        //   effect: 'darken'
+        // FIXME: stroke without fill makes the feature blink while hovering
+        fill: {
+          color: '#ff00ff'
+        },
+        // FIXME: fill without stroke makes the feature blink while hovering
+        stroke: {
+          color: '#000000'
+        }
+      },
+      content: [{ key: 'Layer: MY_VECTOR_LAYER' }, { key: 'Name', valueProperty: 'name' }]
+    }
+  },
+  listing: {
+    layerId: 'MY_LISTED_VECTOR_LAYER',
+    layerInspireName: 'My layer group',
+    layerOrganizationName: 'Organization name',
+    showLayer: true,
+    opacity: 100,
+    layerName: 'Layer name',
+    layerDescription: 'Description text',
+    minZoomLevel: 6
   }
 };
 </script>
