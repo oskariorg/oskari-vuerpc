e5ce98e0cbe00cf86e46cd0299d3ca4de45ba074
AleksiKaa <allu.kaariainen@gmail.com>
2023-06-12 14:06:34 +0300

    move variable 'features' from examplecodes.js

--- src/util/examplecodes.js
+++ src/util/examplecodes.js
@@ -1,67 +1,6 @@
 /*
  *             IN THIS FILE YOU CAN FIND THE EXAMPLES USED IN HIGHLIGHTJS. THE EXAMPLES IN THIS FILE ARE TOO BIG TO BE USED IN TEMPLATE SO THEY ARE IMPORTED FROM HERE INSTEAD
  */
-var features = {
-  // EVENT feature.event.vue
-  event: `{
-    "operation": "add",
-    "features": [
-      {
-        "id": "F24",
-        "geojson": {
-          "type": "FeatureCollection",
-          "features": [
-            {
-              "type": "Feature",
-              "id": "F24",
-              "geometry": {
-                "type": "LineString",
-                "coordinates": [
-                  [
-                    488704,
-                    6939136
-                  ],
-                  [
-                    588704,
-                    7039136
-                  ]
-                ]
-              },
-              "properties": {
-                "test_property": 1,
-                "oskari-cursor": "zoom-in"
-              }
-            }
-          ]
-        },
-        "layerId": "VECTOR"
-      },
-      {
-        "id": "F25",
-        "geojson": {
-          "type": "FeatureCollection",
-          "features": [
-            {
-              "type": "Feature",
-              "id": "F25",
-              "geometry": {
-                "type": "Point",
-                "coordinates": [
-                  488704,
-                  6939136
-                ]
-              },
-              "properties": {
-                "test_property": 2
-              }
-            }
-          ]
-        },
-        "layerId": "VECTOR"
-      }
-    ]
-  }`
-};
 var wkt = `// Define a WKT geometry
 var WKT = 'POLYGON ((358911.7134508261 6639617.669712467, 358911.7134508261 6694516.612323322, 382536.4910289571 6694516.612323322, 382536.4910289571 6639617.669712467, 358911.7134508261 6639617.669712467))';
 
@@ -102,4 +41,4 @@ channel.postRequest('MapModulePlugin.AddFeaturesToMapRequest', [WKT, {
   attributes: attributes
 }]);`;
 
-export { features, wkt };
+export { wkt };
