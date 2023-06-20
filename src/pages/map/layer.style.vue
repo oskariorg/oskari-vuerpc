3c89df57a3c89769bd99b2e0b9c1e5a53181564f
Aleksi Kääriäinen <allu.kaariainen@gmail.com>
2023-06-20 12:16:24 +0300

    change external sld of example

--- src/pages/map/layer.style.vue
+++ src/pages/map/layer.style.vue
@@ -9,18 +9,13 @@
     <DocumentationLink type="request" :apiDoc="apiDocPageRequest">
       Documentation for {{ requestName }}
     </DocumentationLink>
-
-      <CodeSnippet>
+    <CodeSnippet>
 var layerId = {{ layerId }};
 var params = {{ JSON.stringify(params, null, 2) }};
 channel.postRequest('{{requestName}}', [layerId, true, params]);
-      </CodeSnippet>
-      <!-- The example layer has been removed so the example doesn't work, hence commented out -->
-    <!-- p>
-      Click the button to run the code above:
-      <RunExampleButton @click="updateMapLayer">Change map layer style</RunExampleButton>
-    </p -->
-
+    </CodeSnippet>
+    Click the button to run the code above:
+    <RunExampleButton @click="updateMapLayer">Change map layer style</RunExampleButton>
     <h3>Reset external SLD</h3>
     <p>You can reset the style by sending a null SLD_BODY:</p>
     <CodeSnippet>
@@ -28,7 +23,7 @@ var layerId = {{ layerId }};
 var params = {{ JSON.stringify(resetParams, null, 2) }};
 channel.postRequest('{{requestName}}', [layerId, true, params]);
     </CodeSnippet>
-    <!-- RunExampleButton @click="resetMapLayer">Reset map layer style</RunExampleButton -->
+    <RunExampleButton @click="resetMapLayer">Reset map layer style</RunExampleButton>
   </div>
 </template>
 
@@ -38,26 +33,9 @@ const title = 'Change map layer style';
 const requestName = 'MapModulePlugin.MapLayerUpdateRequest';
 const apiDocPageRequest = 'mapping/mapmodule/request/changemaplayerstylerequest.md';
 
-const layerId = 1387;
+const layerId = 1632;
 const params = {
-  SLD_BODY: `<StyledLayerDescriptor xmlns="http://www.opengis.net/sld" xmlns:ogc="http://www.opengis.net/ogc" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" version="1.0.0" xsi:schemaLocation="http://www.opengis.net/sld StyledLayerDescriptor.xsd">
-      <NamedLayer>
-        <Name>oskari:kunnat2013</Name>
-        <UserStyle>
-          <Title>SLD Cook Book: Simple polygon</Title>
-          <FeatureTypeStyle>
-            <Rule>
-              <PolygonSymbolizer>
-                <Fill>
-                  <CssParameter name="fill">#000080</CssParameter>
-                  <CssParameter name="fill-opacity">0.5</CssParameter>
-                </Fill>
-              </PolygonSymbolizer>
-            </Rule>
-          </FeatureTypeStyle>
-        </UserStyle>
-      </NamedLayer>
-    </StyledLayerDescriptor>`
+  SLD_BODY: `<?xml version="1.0" ?><StyledLayerDescriptor version="1.0.0" xmlns="http://www.opengis.net/sld" xmlns:ogc="http://www.opengis.net/ogc" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.opengis.net/sld http://schemas.opengis.net/sld/1.0.0/StyledLayerDescriptor.xsd"><NamedLayer><Name>tieliikenne:tieliikenne_2015</Name><UserStyle><Title>Heatmap</Title><FeatureTypeStyle><Transformation><ogc:Function name="gs:Heatmap"><ogc:Function name="parameter"><ogc:Literal>data</ogc:Literal></ogc:Function><ogc:Function name="parameter"><ogc:Literal>radiusPixels</ogc:Literal><ogc:Function name="env"><ogc:Literal>radius</ogc:Literal><ogc:Literal>30</ogc:Literal></ogc:Function></ogc:Function><ogc:Function name="parameter"><ogc:Literal>pixelsPerCell</ogc:Literal><ogc:Literal>10</ogc:Literal></ogc:Function><ogc:Function name="parameter"><ogc:Literal>outputBBOX</ogc:Literal><ogc:Function name="env"><ogc:Literal>wms_bbox</ogc:Literal></ogc:Function></ogc:Function><ogc:Function name="parameter"><ogc:Literal>outputWidth</ogc:Literal><ogc:Function name="env"><ogc:Literal>wms_width</ogc:Literal></ogc:Function></ogc:Function><ogc:Function name="parameter"><ogc:Literal>outputHeight</ogc:Literal><ogc:Function name="env"><ogc:Literal>wms_height</ogc:Literal></ogc:Function></ogc:Function></ogc:Function></Transformation><Rule><RasterSymbolizer><Geometry><ogc:PropertyName>the_geom</ogc:PropertyName></Geometry><Opacity>1</Opacity><ColorMap type="ramp" ><ColorMapEntry color="#FFFFFF" quantity="0.02" opacity="0" /><ColorMapEntry color="#4444FF" quantity="0.1" opacity="1" /><ColorMapEntry color="#FF0000" quantity="0.5" opacity="1" /><ColorMapEntry color="#FFFF00" quantity="1" opacity="1" /></ColorMap></RasterSymbolizer></Rule></FeatureTypeStyle></UserStyle></NamedLayer></StyledLayerDescriptor>`
 };
 
 const resetParams = {
