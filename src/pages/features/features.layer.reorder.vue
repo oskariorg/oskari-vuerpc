1a25c27d153caa9dfb4eb1bc301989435a899f8d
AleksiKaa <allu.kaariainen@gmail.com>
2023-06-21 15:07:11 +0300

    poc example

--- src/pages/features/features.layer.reorder.vue
+++ src/pages/features/features.layer.reorder.vue
@@ -9,23 +9,7 @@
       iframe.
     </p>
     <CodeSnippet>
-const layer = {
-  layerId: 'MY_LISTED_VECTOR_LAYER',
-  opacity: 75,
-  showLayer: true,
-  layerName: 'My layer',
-  hover: {
-    featureStyle: {
-      fill: {
-        color: '#ff00ff'
-      },
-      stroke: {
-        color: '#000000'
-      }
-    },
-    content: [{ key: 'Layer: MY_LISTED_VECTOR_LAYER' }, { key: 'Name', valueProperty: 'name' }]
-  }
-};
+const layer = {{ layer }}
 
 channel.postRequest('VectorLayerRequest', [layer]);
 channel.log('VectorLayerRequest posted with data', [layer]);
@@ -44,10 +28,30 @@ const layerId = 'MY_VECTOR_LAYER';
 const newPos = 0; // set layer to bottom
 channel.postRequest('RearrangeSelectedMapLayerRequest', [layerId, newPos]);
     </CodeSnippet>
+    <b-dd :text="layers[selectedLayerId]" variant="outline-success" style="width: 250px">
+      <b-dd-item-button
+        v-for="[id, name] in Object.entries(layers)"
+        :key="id"
+        @click="setSelectedLayerId(id)"
+      >
+        {{ name }}
+      </b-dd-item-button>
+    </b-dd>
+    <b-dd :text="selectedPosition.toString()" variant="outline-success" class="dd">
+      <b-dd-item-button
+        v-for="n in Object.keys(layers).length"
+        :key="n"
+        @click="setSelectedPosition(n - 1)"
+      >
+        {{ n - 1 }}
+      </b-dd-item-button>
+    </b-dd>
+    <RunExampleButton @click="reorderLayer">Change ordering</RunExampleButton>
   </div>
 </template>
 <script>
 import { generator } from './vectorlayer_helpers';
+import EVENTBUS from '../../util/eventbus';
 
 const title = 'Reorder vector feature layers';
 const requestName = 'VectorLayerRequest';
@@ -58,17 +62,39 @@ const geojsonObject = generator.getCollectionOf([
   generator.getPolygon(x, y, { name: `I'm a polygon` })
 ]);
 
+const listeners = [];
+
 export default {
   name: 'layerReorder',
   label: title,
   data() {
     return {
       requestName,
-      layer
+      layer,
+      layers: {},
+      selectedLayerId: 801, // default to background map
+      selectedPosition: 0
     };
+  },
+  mounted() {
+    if (this.$root.channel.isReady()) {
+      this.initLayers();
+    } else {
+      listeners.push(
+        EVENTBUS.on('channel.available', () => {
+          this.initLayers();
+        })
+      );
+    }
+  },
+  beforeUnmount() {
+    while (listeners.length) {
+      EVENTBUS.off('channel.available', listeners.pop());
+    }
   },
   methods: {
     addListedLayer() {
+      this.layers[this.layer.layerId] = this.layer.layerName;
       this.$root.channel.postRequest('VectorLayerRequest', [this.layer]);
       this.$root.channel.log('VectorLayerRequest posted with data', [this.layer]);
     },
@@ -76,13 +102,32 @@ export default {
       const params = [geojsonObject, { layerId: this.layer.layerId, clearPrevious: true }];
       this.$root.channel.postRequest('MapModulePlugin.AddFeaturesToMapRequest', params);
       this.$root.channel.log('MapModulePlugin.AddFeaturesToMapRequest posted with data', params);
+    },
+    initLayers() {
+      this.$root.channel.getAllLayers((layers) => {
+        layers.forEach((l) => {
+          this.layers[l.id] = l.name;
+        });
+      });
+    },
+    reorderLayer() {
+      this.$root.channel.postRequest('RearrangeSelectedMapLayerRequest', [
+        this.selectedLayerId,
+        this.selectedPosition
+      ]);
+    },
+    setSelectedLayerId(id) {
+      this.selectedLayerId = id;
+    },
+    setSelectedPosition(n) {
+      this.selectedPosition = n;
     }
   }
 };
 
 const layer = {
   layerId: 'MY_LISTED_VECTOR_LAYER',
-  opacity: 75,
+  opacity: 100,
   showLayer: true,
   layerName: 'My vector layer',
   hover: {
