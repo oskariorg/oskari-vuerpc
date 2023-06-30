639f6949c4c18222bd820502245a513b4cc4f2a3
AleksiKaa <allu.kaariainen@gmail.com>
2023-06-30 09:43:27 +0300

    init code editor component

---
+++ src/components/ui-components/codeeditor.vue
@@ -0,0 +1,33 @@
+<template>
+  <div :id="id" class="editor"></div>
+</template>
+<script>
+import ace from 'ace-builds';
+import 'ace-builds/esm-resolver';
+
+export default {
+  name: 'codeEditor',
+  data() {
+    return {
+      id: `${this.$.uid}`
+    };
+  },
+  mounted() {
+    const editor = ace.edit(this.id, {
+      maxLines: 20,
+      minLines: 10,
+      fontSize: 14,
+      theme: 'ace/theme/one_dark',
+      tabSize: 4
+    });
+    editor.session.setMode('ace/mode/typescript');
+  }
+};
+</script>
+
+<style>
+.editor {
+  width: 100%;
+  height: 400px;
+}
+</style>
