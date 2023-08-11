4b1a3aab4fdf3d137c658840fccbf8f2a3999a02
Aleksi Kääriäinen <allu.kaariainen@gmail.com>
2023-08-11 14:50:46 +0300

    simplify event handling in features.info

--- src/pages/features/features.info.vue
+++ src/pages/features/features.info.vue
@@ -50,7 +50,6 @@ import EVENTBUS from '../../util/eventbus.js';
 const title = 'Get feature info';
 const requestName = 'MapModulePlugin.GetFeatureInfoRequest';
 const gfiLayerId = 492;
-const listeners = [];
 
 export default {
   name: 'GetFeatureInfoRequest',
@@ -86,19 +85,14 @@ export default {
     if (this.$root.channel.isReady()) {
       this.mapLayerVisibilityRequest(true);
     } else {
-      listeners.push(
-        EVENTBUS.on('channel.available', () => {
-          this.mapLayerVisibilityRequest(true);
-        })
-      );
+      EVENTBUS.once('channel.available', () => {
+        this.mapLayerVisibilityRequest(true);
+      });
     }
   },
   beforeUnmount() {
     this.$root.channel.postRequest('InfoBox.HideInfoBoxRequest');
     this.mapLayerVisibilityRequest(false);
-    while (listeners.length) {
-      EVENTBUS.off('channel.available', listeners.pop());
-    }
   }
 };
 </script>
