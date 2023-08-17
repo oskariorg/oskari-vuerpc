8c6011eea38bd1efe1af56cabe5512e69bb7f9a9
Aleksi Kääriäinen <allu.kaariainen@gmail.com>
2023-08-17 12:05:21 +0300

    add some styling to nav links

--- src/components/ui-components/page_navigation.vue
+++ src/components/ui-components/page_navigation.vue
@@ -4,10 +4,16 @@
       <h2 v-if="!hideSelect">Select Action <NavigationDropdown :current="currentPage.name" /></h2>
       <div class="navigationLinkParent">
         <div class="prev" v-if="navLinks.prev">
-          <router-link :to="navLinks.prev.path">Previous: {{ navLinks.prev.label }}</router-link>
+          <router-link class="navigationLink" :to="navLinks.prev.path">
+            <i class="arrow left"></i>
+            Previous: {{ navLinks.prev.label }}
+          </router-link>
         </div>
         <div class="next" v-if="navLinks.next">
-          <router-link :to="navLinks.next.path">Next: {{ navLinks.next.label }}</router-link>
+          <router-link class="navigationLink" :to="navLinks.next.path">
+            Next: {{ navLinks.next.label }}
+            <i class="arrow right"></i>
+          </router-link>
         </div>
       </div>
     </div>
@@ -56,9 +62,20 @@ export default {
 }
 
 .navigationLinkParent {
-  padding-left: 0.7em;
-  padding-right: 0.7em;
+  padding: 0.7em;
   overflow: hidden;
+}
+
+.navigationLink {
+  padding: 0.5em;
+  border: solid 1px black;
+  border-radius: 5px;
+  background-color: aqua;
+}
+
+.navigationLink:hover {
+  text-decoration: none;
+  background-color: blue;
 }
 
 .prev {
@@ -67,5 +84,20 @@ export default {
 
 .next {
   float: right;
+}
+
+.arrow {
+  border: solid black;
+  border-width: 0 3px 3px 0;
+  display: inline-block;
+  padding: 3px;
+}
+
+.left {
+  transform: rotate(135deg);
+}
+
+.right {
+  transform: rotate(-45deg);
 }
 </style>
