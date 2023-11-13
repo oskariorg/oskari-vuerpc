d918bf754c9ea9e02a6357d659ea694e0ef72c85
Aleksi Kääriäinen <allu.kaariainen@gmail.com>
2023-11-13 12:07:33 +0200

    mock navbar replicating the style of new oskari website look

--- src/components/ui-components/Header.vue
+++ src/components/ui-components/Header.vue
@@ -1,24 +1,26 @@
 <template>
   <div class="header">
-    <div class="nav-content">
-      <div class="oskari-logo">
-        <a href="http://www.oskari.org"
-          ><img src="../../assets/img/oskari_logo_white.png" style="height: 65px"
-        /></a>
+    <div class="nav-container">
+      <div class="nav-content">
+        <div class="oskari-logo">
+          <a href="http://www.oskari.org"
+            ><img src="../../assets/img/oskari_logo_black_horizontal.svg" style="height: 3rem"
+          /></a>
+        </div>
+        <nav>
+          <ul id="nav" role="navigation">
+            <li><a href="/guides">Guides</a></li>
+            <li><a href="/documentation">Documentation</a></li>
+            <li><a href="/gallery">Gallery</a></li>
+            <li><a href="/download">Download</a></li>
+            <li><a href="/about">About</a></li>
+          </ul>
+        </nav>
+        <button id="nav-download">Download</button>
       </div>
-      <nav>
-        <ul id="nav navbar-nav" role="navigation">
-          <li><a href="/guides">Guides</a></li>
-          <li><a href="/documentation">Documentation</a></li>
-          <li><a href="/gallery">Gallery</a></li>
-          <li><a href="/download">Download</a></li>
-          <li><a href="/about">About</a></li>
-        </ul>
-      </nav>
     </div>
   </div>
 </template>
-
 <script>
 export default {
   name: 'pageHeader',
@@ -28,46 +30,67 @@ export default {
 };
 </script>
 <style>
-.header {
-  width: 100%;
-  background: #262626;
-  border-bottom: 1px solid maroon;
-  -webkit-box-shadow: 0 5px 20px 0 rgba(0, 0, 0, 0.3);
-  box-shadow: 0 5px 20px 0 rgba(0, 0, 0, 0.3);
-}
-.header nav {
-  font-size: calc(0.6rem + 1vh);
-}
-.header nav ul li {
-  display: inline-block;
-  margin-left: 0.5em;
-  list-style-type: none;
-  padding-bottom: 5px;
-  border-top: 15px solid transparent;
+.nav-container {
+  max-width: 82rem; /*Hard-coding not optimal*/
+  font-family: 'League Spartan', Helvetica, sans-serif;
+  padding-left: 1.5rem;
+  padding-right: 1.5rem;
+  height: 80px
 }
 .nav-content {
-  max-width: 1200px;
-  margin-right: auto;
+  max-width: 1920px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 7fr auto;
+  align-items: center;
+  gap: 1rem;
+  height: auto;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+}
+.nav-container,
+.nav-content {
   margin-left: auto;
-  padding: 0 0 2em;
-  height: 60px;
+  margin-right: auto;
+  width: 100%;
+}
+#nav {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  font-weight: 700;
+  gap: 1.5rem;
+  float: right;
+  margin-bottom: 0;
+  padding-right: 1.5rem;
+}
+#nav a,
+#nav a:hover {
+  text-decoration: none;
+  color: #222222;
+}
+#nav-download {
+  height: auto;
+  background-color: #ffd400;
+  color: #222222;
+  float: right;
+  border-radius: 50rem;
+  border-color: transparent;
+  font-weight: 700;
+  padding: 0.8rem 1.6rem;
+}
+.nav-download:hover {
+  text-decoration: none;
+  background-color: #ffd400;
+  color: #222222;
+}
+a:hover {
+  color: #222222;
 }
 .oskari-logo {
-  float: left;
-}
-
-#header-items {
-  margin: auto;
-  font-size: 1.2em;
-  display: inline-block;
-}
-.header li {
-  margin-left: 0.5em;
-  display: inline-block;
-}
-.header a {
-  color: #e8ecef;
-  padding-left: 0.7em;
-  padding-right: 0.7em;
+  display: flex;
+  justify-content: flex-start;
+  vertical-align: baseline;
 }
 </style>
